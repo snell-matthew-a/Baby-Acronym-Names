2ecfa2e6a20221b1b72615cf91ff40403d90afdb
Matt Snell <snell.matthew.a@gmail.com>
2026-03-30 14:32:35 -0400

    Streamlined dialogue.  Show user list of names before prompting to save

--- names.py
+++ names.py
@@ -29,7 +29,7 @@ def tally_names(names_folder):
     print("Data successfully saved to 'tally_names.csv'")
 
 
-def validate_names(baby_gender, source_names, names_file=names_file, save=True):
+def validate_names(baby_gender, source_names, names_file=names_file):
     """
     Validates baby names against a set of source names and saves matching names to a CSV file.
 
@@ -46,10 +46,6 @@ def validate_names(baby_gender, source_names, names_file=names_file, save=True):
     df = df[df["name"].apply(lambda x: "".join(sorted(x.lower())) in match_set)]
     df = df.drop(columns="gender")
     df = df.sort_values(by="count", ascending=False).reset_index(drop=True)
-
-    if save:
-        df.to_csv("matching_names.csv", index=False)
-        print("Matching names successfully saved to 'matching_names.csv'")
 
     return df
 
@@ -91,22 +87,32 @@ def get_names(prompt="Enter the source names (comma-separated): "):
     # Prompt the user to input the source names
     while True:
         value = input(prompt).strip()
+        if value == "Q":
+            print("Exiting the program.")
+            exit()
         if value:
             source_names = [name.strip() for name in value.split(",")]
             return source_names
         print("Invalid input. Please enter at least one name.")
 
 
-def get_save(prompt="Do you want to save the matching names to a CSV file? (Y/N): "):
+def get_save(n=101, prompt="Do you want to save your results? (Y/N): "):
+    if n > 100:
+        prompt = "Do you want to save your results to see the rest? (Y/N): "
     # Prompt the user to decide whether to save the matching names
     while True:
         value = input(prompt).strip().upper()
         if value in ["Y", "N"]:
             return value == "Y"
+        if value == "Q":
+            print("Exiting the program.")
+            exit()
         print("Invalid input. Please enter 'Y' for yes or 'N' for no.")
 
 
 if __name__ == "__main__":
+    print("\nWelcome to the Baby Name Matcher!")
+
     if not os.path.exists(names_file):
         print(f"Tallying names from source files...")
         tally_names(names_folder)
@@ -114,12 +120,23 @@ if __name__ == "__main__":
     # Prompt the user for baby gender, source names, and whether to save the results
     baby_gender = get_gender()
     source_names = get_names()
-    save = get_save()
 
     # Validate the names and store in a dataframe
-    names = validate_names(baby_gender, source_names, save=save)
+    names = validate_names(baby_gender, source_names)
 
-    # Print the matching names    print("\nMatching Names:")
-    print(f"{len(names)} matching names found, here are the top 100:")
-    with pd.option_context("display.max_rows", 100, "display.max_columns", None):
+    # Print the matching names and ask the user if they want to save the results
+    with pd.option_context("display.max_rows", 100):
         print(names.head(100))
+
+    if len(names) > 100:
+        print(
+            f"\nWe found {len(names)} matching names in total - here are the top 100!\n"
+        )
+    else:
+        print(f"\nWe found {len(names)} matching names in total!\n")
+
+    if get_save(len(names)):
+        names.to_csv("matching_names.csv", index=False)
+        print("Your names have been saved to 'matching_names.csv'!")
+
+    print("Thank you for using the Baby Name Matcher! Goodbye!")
